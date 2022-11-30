1230311b95be868327749a7f825e66a6162d71f8
Otho TK <otho.tk@hotmail.com>
2022-11-30 19:18:31 -0300

    🎨 Cormen example

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,17 @@ fn main() {
         ]),
     };
 
-    let (dist, path) = shortest_path(graph, 0, 4);
+    let graphCormen = Graph {
+        vertexes: Vec::from([
+            LinkedList::from([(1, 10), (4, 5)]), // 0
+            LinkedList::from([(2, 1), (4, 2)]),  // 1
+            LinkedList::from([(3, 4)]),          // 2
+            LinkedList::from([(0, 7), (2, 6)]),  // 3
+            LinkedList::from([(1,3),(2,9),(3,2)]),              // 4
+        ]),
+    };
+
+    let (dist, path) = shortest_path(graphCormen, 0, 1);
 
     println!("best dist = {}", dist);
     print!("Shortest path = ");
